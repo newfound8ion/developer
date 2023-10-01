aa991db6778554281983bcafe49ea1184a88ddd1
Vectoshi Eigenvector Newkamoto <112469623+vectoshi@users.noreply.github.com>
2023-10-01 13:38:04 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,12 +54,12 @@ export default function Home(): JSX.Element {
             marginTop: '0', 
           }} 
         />
-
+<div ClassName="row">
          <h2>Robust</h2> <p>Newcoin is designed to interface with diverse systems, handling varied input data types and formats. By strictly regulating output while accommodating a broad spectrum of inputs, networks achieve consistent and clear communication across nodes.</p>
           <h2>Adaptive</h2> <p> As Newcoin collects data signals, the Proof-of-Creativity algorithm recursively adjusts its centrality measurement and makes the next computation more accurate and relevant.</p>
           <h2>Synergistic</h2> <p>  Newcoin is designed to mutualize data and computation into a shared intelligence greater than the sum of their separate intelligence. A like on social media enhances the decentralization of a consensus mechanism which in turn fuels better data inputs to ML pipelines.</p>
-
-        <div className={styles.buttons}>
+       </div>
+ <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/getting-started/overview">
