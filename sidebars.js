fb1e9fb325515283ad3b6186319361e0e35d825e
Vectoshi Eigenvector Newkamoto <112469623+vectoshi@users.noreply.github.com>
2024-03-09 19:50:32 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -8,12 +8,29 @@ const sidebars = {
       type: "category",
       label: "Core Concepts",
       items: [
+        "core-concepts/ipsp",
         "core-concepts/WATT",
         "core-concepts/neural-tokens",
         "core-concepts/encoder",
         "core-concepts/activation-function",
       ],
     },
+    {
+      type: "category",
+      label: "Getting Started",
+      items: [
+        "getting-started/overview",
+        {
+          type: "category",
+          label: "Creating Watts",
+          items: [
+            "getting-started/creating-watts/activation-function-interface",
+            "getting-started/creating-watts/activation-function-logic",
+            "getting-started/creating-watts/register-activation-function",
+            "getting-started/creating-watts/activation-function-approval",
+            "getting-started/creating-watts/call-activate",
+          ],
+        },
     {
       type: "category",
       label: "Getting Started",
