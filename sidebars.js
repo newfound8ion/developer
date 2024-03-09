e43d38c02ea76608d65013941ed31bbf051bba33
Vectoshi Eigenvector Newkamoto <112469623+vectoshi@users.noreply.github.com>
2024-03-09 23:17:01 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -14,22 +14,6 @@ const sidebars = {
         "core-concepts/base-points",
       ],
     },
-    {
-      type: "category",
-      label: "Getting Started",
-      items: [
-        "getting-started/overview",
-        {
-          type: "category",
-          label: "Creating Watts",
-          items: [
-            "getting-started/creating-watts/activation-function-interface",
-            "getting-started/creating-watts/activation-function-logic",
-            "getting-started/creating-watts/register-activation-function",
-            "getting-started/creating-watts/activation-function-approval",
-            "getting-started/creating-watts/call-activate",
-          ],
-        },
         {
           type: "category",
           label: "Using Watts",
